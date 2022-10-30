e58ae9a04e67dca585a29ac56bb187179c2b41e8
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-10-30 18:53:17 +0100

    test to see if everyhting works

--- front/MyJS/index.js
+++ front/MyJS/index.js
@@ -1,7 +1,30 @@
-fetch('http://localhost:3000/api/products')
- .then(function(response){
-    return response.json();
- })
- .then(function(data){
-    console.log(data)
- })
+/* Global Variables */
+const url = 'http://localhost:3000/api/products/';
+const itemsBox = document.getElementById('items');
+
+// Initialise la page index.html
+loadProducts();
+
+/* *********** *
+ *  Fonctions  *
+ * *********** */
+// Chargement et mise en page.
+function loadProducts() {
+  fetch(url).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((data) => {
+    data.forEach(product => {
+      // Creation du lien de l'article.
+      let myLink = document.createElement("a");
+      myLink.setAttribute("href", `./product.html?id=${product._id}`);
+      myLink.innerHTML = `<article><img src="${product.imageUrl}" alt="${product.altTxt}"><h3 class="productName">${product.name}</h3><p class="productDescription">${product.description}</p></article>`;
+      itemsBox.appendChild(myLink);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+}
