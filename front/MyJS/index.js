cc65c4064e05e62b67b7ba3f1927011576ebc7b1
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-02 15:21:22 +0100

    allez

--- front/MyJS/index.js
+++ front/MyJS/index.js
@@ -1,41 +1,9 @@
- /* Global Variables */
-/*const url = 'http://localhost:3000/api/products/';
-const itemsBox = document.getElementById('items');
-
-// Initialise la page index.html
-loadProducts();
-
-// Chargement et mise en page.
-function loadProducts() {
-  fetch(url).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then((data) => {
-    data.forEach(product => {
-      // Creation du lien de l'article.
-      let myLink = document.createElement("a");
-      myLink.setAttribute("href", `./product.html?id=${product._id}`);
-      myLink.innerHTML = `<article>
-      <img src="${product.imageUrl}" alt="${product.altTxt}">
-      <h3 class="productName">${product.name}</h3>
-      <p class="productDescription">${product.description}</p>
-      </article>`;
-      itemsBox.appendChild(myLink);
-    });
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-}*/
-
 let urlProductsData = 'http://localhost:3000/api/products/' ;
 let idToPutProductsData = document.getElementById('items') ;
 
-loadProducts();
+dynamicProducts();
 
-function loadProducts() {
+function dynamicProducts() {
   fetch(urlProductsData).then((response) => {
     if (response.ok) {
       return response.json();
@@ -44,11 +12,18 @@ function loadProducts() {
   .then((data) => {
     data.forEach(product => {
       let example = document.createElement("itemsContent");
+
+      /*  Il reste à insérer le lien vers la page du produit : <a href="./product.html?id=42">
+      * donc surement avec ce lien : ./product.html?id=${product._id}
+      *
+      */
+      
       example.innerHTML = `<article>
       <img src="${product.imageUrl}" alt="${product.altTxt}">
       <h3 class="productName">${product.name}</h3>
       <p class="productDescription">${product.description}</p>
       </article>`
+
       idToPutProductsData.appendChild(example);
     }
       )
